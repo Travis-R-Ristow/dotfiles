244d1f60d79b631ce520816b2d77dbd3ed31d3bf
Travis Ristow <travis.ristow@carvana.com>
2025-07-28 13:11:28 -0700

    fix copy past

--- nvim/init.lua
+++ nvim/init.lua
@@ -20,8 +20,13 @@ opt.spelloptions = "camel"
 -- KeyBind / Remaps
 -- vim.keymap.set("n", "<Home>", "^", { silent = true })
 -- vim.keymap.set("n", "<End>", "$", { silent = true })
-vim.keymap.set("v", "Y", '"zy')
-vim.keymap.set("n", "P", '"zp')
+
+vim.keymap.set({ "v", "n" }, "y", '"zy')
+vim.keymap.set({ "v", "n" }, "p", '"zp')
+vim.keymap.set({ "v", "n" }, "P", '"0p')
+
+vim.keymap.set({ "v", "n" }, "<leader>yl", ":registers<CR>")
+
 vim.keymap.set("n", "grn", vim.lsp.buf.rename)
 
 vim.api.nvim_create_user_command("W", function()
@@ -38,6 +43,10 @@ end, {})
 
 vim.api.nvim_create_user_command("WQ", function()
 	vim.cmd("wq")
+end, {})
+
+vim.api.nvim_create_user_command("Wall", function()
+	vim.cmd("wall")
 end, {})
 
 vim.cmd("let g:netrw_liststyle = 3")
