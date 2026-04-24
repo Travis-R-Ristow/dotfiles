f6a36c0b4cd0f9a65fd1be77ee0b2e5bb78b1554
Travis Ristow <travis.ristow@carvana.com>
2026-04-24 10:46:49 -0700

    update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
-	dependencies = {},
 	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-		treesitter.setup({
-			indent = { enable = true },
-			autotag = { enable = true },
-			sync_install = true,
-			ensure_installed = {
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"tsx",
-				"lua",
-				"graphql",
-				"c_sharp",
-				"markdown",
-			},
-			auto_install = true,
-			ignore_install = {},
-			modules = {},
-			highlight = {
-				enable = true,
-			},
-		})
-
 		vim.opt.foldmethod = "manual"
 		vim.opt.foldtext = ""
 	end,
